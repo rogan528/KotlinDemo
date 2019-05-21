69491bae45eaf67d3a9fa0d41b20db943e82153b
rogan528 <3097940262@qq.com>
2019-05-21 22:22:24 +0800

    修改

--- app/src/main/java/com/zhangbin/kotlin/MainActivity.kt
+++ app/src/main/java/com/zhangbin/kotlin/MainActivity.kt
@@ -2,11 +2,17 @@ package com.zhangbin.kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+        tv_show.text="hello world"
     }
 }
