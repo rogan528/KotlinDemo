ec5b269b7a28fae82de425ec75614098b31d3671
rogan528 <3097940262@qq.com>
2019-05-22 13:42:25 +0800

    混淆代码

---
+++ app/src/main/java/com/zhangbin/kotlin/bean/User.java
@@ -0,0 +1,17 @@
+package com.zhangbin.kotlin.bean;
+
+/**
+ * @Description:
+ * @Author: 张彬
+ * @CreateDate: 2019-05-22
+ * @QQ: 3097940262
+ */
+public class User {
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String name;
+    public int age;
+}
